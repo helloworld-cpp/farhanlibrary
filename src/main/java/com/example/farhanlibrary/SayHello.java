447729942a58a67ed5c4291879dd99feb339175c
sheikh.farhan <farhansheikhfs@gmail.com>
2022-11-13 15:51:38 +0530

    hello

---
+++ src/main/java/com/example/farhanlibrary/SayHello.java
@@ -0,0 +1,10 @@
+package com.example.farhanlibrary;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class SayHello {
+    public static String sayHello(){
+        return "Hello i am farhan, saying from custom library";
+    }
+}
